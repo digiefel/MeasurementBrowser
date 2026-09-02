0b6e260ea4b37e71b30060992f92759379f7e502
Davide Lombardo <dlombardo32@gmail.com>
2026-09-02 18:31:02 +0200

    better format test log output

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,26 +11,39 @@ atexit(() -> rm(TEST_CACHE_DEPOT; force=true, recursive=true))
 
 include(joinpath(@__DIR__, "test_project.jl"))
 
-@testset "DataBrowser Tests" begin
-    include(joinpath(@__DIR__, "test_project_view_state.jl"))
-    include(joinpath(@__DIR__, "test_work_graph.jl"))
-    include(joinpath(@__DIR__, "test_collection_cache.jl"))
-    include(joinpath(@__DIR__, "test_hierarchy_edit.jl"))
-    include(joinpath(@__DIR__, "test_open_options.jl"))
-    include(joinpath(@__DIR__, "test_label_cache.jl"))
-    include(joinpath(@__DIR__, "test_public_api.jl"))
-    include(joinpath(@__DIR__, "test_wide_cache.jl"))
-    include(joinpath(@__DIR__, "test_collection_identity.jl"))
-    include(joinpath(@__DIR__, "test_collection_index.jl"))
-    include(joinpath(@__DIR__, "test_collection_id_persistence.jl"))
-    include(joinpath(@__DIR__, "test_type_api.jl"))
-    include(joinpath(@__DIR__, "test_table_inspector.jl"))
-    include(joinpath(@__DIR__, "test_debug_tools.jl"))
-    include(joinpath(@__DIR__, "test_timing.jl"))
-    include(joinpath(@__DIR__, "test_gui_extensions.jl"))
-    include(joinpath(@__DIR__, "test_plots_extension_boundary.jl"))
-    include(joinpath(@__DIR__, "test_annotations.jl"))
+const UNIT_TEST_FILES = (
+    "test_project_view_state.jl",
+    "test_work_graph.jl",
+    "test_collection_cache.jl",
+    "test_hierarchy_edit.jl",
+    "test_open_options.jl",
+    "test_label_cache.jl",
+    "test_public_api.jl",
+    "test_wide_cache.jl",
+    "test_collection_identity.jl",
+    "test_collection_index.jl",
+    "test_collection_id_persistence.jl",
+    "test_type_api.jl",
+    "test_table_inspector.jl",
+    "test_debug_tools.jl",
+    "test_timing.jl",
+    "test_gui_extensions.jl",
+    "test_plots_extension_boundary.jl",
+    "test_annotations.jl",
+)
+
+@testset verbose = true "package tests" begin
+    for file in UNIT_TEST_FILES
+        @testset "$file" begin
+            include(joinpath(@__DIR__, file))
+        end
+    end
 end
 
 include(joinpath(@__DIR__, "..", "bench", "run.jl"))
-run_performance()
+
+@testset "performance snapshot" begin
+    redirect_stdout(run_performance, devnull)
+    @test isfile(STATUS_PATH) && filesize(STATUS_PATH) > 0
+    println("Wrote ", STATUS_PATH)
+end
